685f1e4642fb1078d3aa323bab28368469f8c856
S Alankritha <you@example.coms.alankritha@mca.christuniversity.in>
2024-11-30 21:44:19 +0530

    Initial commit

---
+++ src/assets/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+    return(
+        <div>
+            <nav>
+                <div className="logo">
+                    <img src="" alt="" />
+                </div>
+
+            </nav>
+        </div>
+    )
+}
+export default App;
